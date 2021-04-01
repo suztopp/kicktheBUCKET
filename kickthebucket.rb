43176f61a610339ed6e69850da1b3d60f8ff9c00
suztopp <suzannetopp81@gmail.com>
2021-04-01 15:41:18 +1100

    wrote welcome method

---
+++ kickthebucket.rb
@@ -0,0 +1,4 @@
+require_relative 'lib/app'
+
+todo = App.new
+todo.run
--- lib/app.rb
+++ lib/app.rb
@@ -4,4 +4,10 @@ class App
         @activities = []
     end
 
+    def display_welcome
+        puts "Welcome to:"
+        puts "KICK THE BUCKET!!"
+        puts "Your one stop destination for making"
+        puts "10 POST COVID dreams come to life!"
+    end
 end
