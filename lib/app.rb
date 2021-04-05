0052e31453d6f4b8aa3661e7fb86cae8710ba8be
suztopp <suzannetopp81@gmail.com>
2021-04-06 09:59:23 +1000

    thanksfully got the display working with a class for the activities

--- lib/app.rb
+++ lib/app.rb
@@ -2,17 +2,20 @@ require 'json'
 require 'artii'
 
 
+$activities = []                        #global
 
 
 
 class Activities
+
     attr_accessor :activity_name, :time_needed, :activity_reason, :ticked
-    def initialize (activity_name, time_needed, reason)
+    def initialize (activity_name, time_needed, activity_reason)
         @activity_name = activity_name
         @time_needed = time_needed
-        @reason = reason
-        @ticked = false
+        @activity_reason = activity_reason
+        @ticked = false                         #is this right??
     end
+
 end
 
 
@@ -20,34 +23,33 @@ end
 
 
 class App
-    attr_accessor :activities
     
-    def initialize
-        # @activities = []
-        # @file_path = file_path
-        # load_data(file_path)
-    end
-
+    # def initialize
+    #     # @activities = []
+    #     # @file_path = file_path
+    #     # load_data(file_path)
+    # end
 
     # def run
 
     # end
-    activities = []
 
     def run_normal
         loop do
             system 'clear'
             display_welcome
-            puts "---" * 30
+            puts "---" * 40
             display_menu
             menu_selector(select_menu)
         end
     end
 
+
+
     def menu_selector(option_select)
         case option_select
         when 1
-            until @activities.length == 2 do
+            until $activities.length == 3 do
                 system 'clear'
                 display_add_activity
             end 
@@ -62,7 +64,7 @@ class App
         when 3
             system 'clear'
             display_activities
-            puts 'Enter Your Choice Below for ACTIVITY TO EDIT'
+            puts 'Enter Your Number Choice Below for ACTIVITY TO EDIT'
             index = select_activity
             display_add_activity
             #edit activities
@@ -76,6 +78,8 @@ class App
         end
     end
 
+
+
     def display_welcome
         puts "Welcome to:"
         a = Artii::Base.new
@@ -87,6 +91,8 @@ class App
         puts "10 POST COVID dreams come to life!"
         puts "---" * 40
     end
+
+
 
     def display_menu
         puts 'KICK THE BUCKET OPTIONS:'
@@ -101,9 +107,13 @@ class App
         puts "---" * 40
     end
 
+
+
     def select_menu
         gets.to_i
     end
+
+
 
     # def load_data(file_path)
     #     json_data = JSON.parse(File.read(file_path))
@@ -116,10 +126,14 @@ class App
     #     @activities << { activity: activity_input, time_needed: activity_input, activity_reason: activity_input, ticked: false, }
     # end
 
+
+
     def display_add_activity
+
         puts "---" * 40
         a = Artii::Base.new
         puts a.asciify('ACTIVITY INFO')
+
         print 'Enter your Dream Bucket List Activity:  '
         activity_name = gets.chomp
         print 'Enter the TIME GOAL for your Bucket List Activity (ie. one year, five years):  '
@@ -132,33 +146,47 @@ class App
         #     reason: activity_reason,
         #     ticked: false
         # ]
-        activity - new activities(activity_name, time_needed, activity_reason)
+        activity = Activities.new(activity_name,time_needed,activity_reason)
+        $activities <<  activity                
+        #push new instances of activity into Activities array
+
     end
 
     # def activity_info
     #     gets.strip
     # end
 
-    def display_activity(activity)
 
+
+    def display_activity(activity,index)
+        #     puts "#{index+1} Activity: #{activity[:activity]}"
+        puts "#{index+1}. Activity: #{activity.activity_name} - Time Needed: #{activity.time_needed} - Reason: #{activity.activity_reason}"
     end
     
+
+
     def display_activities
         a = Artii::Base.new
         puts a.asciify('BUCKET LIST ACTIVITIES')
-        # @activities.each_with_index do |activity,index|
-        #     puts "#{index+1} Activity: #{activity[:activity]}"
-        while 
+        
+        for i in 0...$activities.length                     #index 0 to last index including
+            display_activity($activities[i],i)              #uses display_activity method to output each index for the Activities array, along with details
         end
     end
 
-    def edit_activity(edited_activity, index)
-        @activities[index][:activity] = edited_activity
-    end
+
+
+    # def edit_activity(edited_activity, index)
+    #     $activities[index][:activity] = edited_activity
+    # end
+
+
 
     def select_activity
         gets.to_i - 1
     end
+
+
 
     def bucket_logo
         puts "
